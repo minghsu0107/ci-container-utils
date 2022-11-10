278cee7220327968193a1b4e94ce54f1f64ec386
Hao-Ming, Hsu <50090692+minghsu0107@users.noreply.github.com>
2022-11-10 18:20:35 +0800

    add comment

--- kubectl-kustomize/kustomization-remote-ref.sh
+++ kubectl-kustomize/kustomization-remote-ref.sh
@@ -23,4 +23,7 @@ yq -i '.resources[0] = "'$REMOTE_URL'?ref='$REF'"' kustomization.yaml
 git config --global user.name $MANIFEST_USER
 git config --global user.email $MANIFEST_USER_EMAIL
 git add . && git commit --allow-empty -m "🚀 update to ${IMAGE_TAG}"
+
+# push the current branch to its upstream branch
+# but refuses to push if the upstream branch's name is different from the local one
 git push $MANIFEST_SSH_URL
