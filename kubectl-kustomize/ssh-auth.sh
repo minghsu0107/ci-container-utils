74d5925076985e7872b57198751bd755a2e57dce
Hao-Ming, Hsu <50090692+minghsu0107@users.noreply.github.com>
2022-12-13 02:52:42 +0800

    Update ssh-auth.sh

--- kubectl-kustomize/ssh-auth.sh
+++ kubectl-kustomize/ssh-auth.sh
@@ -7,5 +7,5 @@ export SSH_KEY_PATH=${SSH_KEY_PATH:-""}
 export GIT_HOST=${GIT_HOST:-""}
 
 mkdir -p ~/.ssh
-cat $SSH_KEY_PATH > ~/.ssh/id_rsa && chmod 700 ~/.ssh/id_rsa
+cat $SSH_KEY_PATH > ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
 ssh-keyscan $GIT_HOST >> ~/.ssh/known_hosts
